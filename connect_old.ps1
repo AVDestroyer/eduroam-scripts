a142f0c76e036d60a79899d1fdc1e15e66d3b2b8
Arnav Vora <42781218+AVDestroyer@users.noreply.github.com>
2025-10-07 14:27:22 -0700

    increase connect delay time

--- connect_old.ps1
+++ connect_old.ps1
@@ -3,9 +3,10 @@ while ($true) {
 	while (-not $result.PingSucceeded) {
 		netsh wlan connect eduroam
 		echo "Tried to reconnect"
-		Start-Sleep -Seconds 5
+		Start-Sleep -Seconds 15
 		$result = Test-NetConnection 8.8.8.8
 	}
 	echo "Connection is OK"
 	Start-Sleep -Seconds 15
+
 }
